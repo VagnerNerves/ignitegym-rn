b94dfef432a6b5eac57fac6fa401536f95f83c51
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-19 16:54:17 -0300

    feat: Add Button in screen,

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import { VStack, Image, Text, Center, Heading } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 
 export function SignIn() {
   return (
@@ -34,6 +35,8 @@ export function SignIn() {
         />
 
         <Input placeholder="Senha" secureTextEntry />
+
+        <Button title="Acessar" />
       </Center>
     </VStack>
   )
