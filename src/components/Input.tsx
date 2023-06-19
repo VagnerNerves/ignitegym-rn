d4430cfa7d74aad0ab355590325ce707ab8cf1f3
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-19 16:06:19 -0300

    feat: Created Component Input.

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { Input as NativeBaseInput, IInputProps } from 'native-base'
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      bg="gray.700"
+      h={14}
+      px={4}
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      fontFamily="body"
+      mb={4}
+      placeholderTextColor="gray.300"
+      _focus={{
+        bg: 'gray.700',
+        borderWidth: 1,
+        borderColor: 'green.500'
+      }}
+      {...rest}
+    />
+  )
+}
