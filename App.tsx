fc721bb76a9158e1645628465c95834ae2380860
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-19 14:34:10 -0300

    feat: ajusted app for screen SignIn.

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import {
 import { THEME } from './src/theme'
 
 import { Loading } from '@components/Loading'
+import { SignIn } from '@screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -22,7 +23,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
