ba00cf39b711cffb67e3764d23c0182637d1f0d2
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-19 18:11:39 -0300

    feat: Ajusted for IOS Scrol with KeyboardAvoidingView.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,13 @@
 import { Platform } from 'react-native'
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  KeyboardAvoidingView
+} from 'native-base'
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
@@ -8,52 +16,58 @@ import { Button } from '@components/Button'
 
 export function SignIn() {
   return (
-    <ScrollView
-      contentContainerStyle={{
-        flexGrow: 1
-      }}
-      showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+      flex={1}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <VStack flex={1} bg="gray.700" px={10}>
-        <Image
-          source={BackgroundImg}
-          alt="Pessoas Treinando"
-          resizeMode="contain"
-          position="absolute"
-        />
-
-        <Center my={24}>
-          <LogoSvg />
-
-          <Text color="gray.100" fontSize="sm">
-            Treine sua mente e o seu corpo
-          </Text>
-        </Center>
-
-        <Center>
-          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
-          </Heading>
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+      <ScrollView
+        bg="gray.700"
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <VStack flex={1} bg="gray.700" px={10}>
+          <Image
+            source={BackgroundImg}
+            alt="Pessoas Treinando"
+            resizeMode="contain"
+            position="absolute"
           />
 
-          <Input placeholder="Senha" secureTextEntry />
+          <Center my={24}>
+            <LogoSvg />
 
-          <Button title="Acessar" />
-        </Center>
+            <Text color="gray.100" fontSize="sm">
+              Treine sua mente e o seu corpo
+            </Text>
+          </Center>
 
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
+          <Center>
+            <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+              Acesse sua conta
+            </Heading>
 
-          <Button title="Criar conta" variant="outline" />
-        </Center>
-      </VStack>
-    </ScrollView>
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+
+            <Input placeholder="Senha" secureTextEntry />
+
+            <Button title="Acessar" />
+          </Center>
+
+          <Center mt={24}>
+            <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+              Ainda não tem acesso?
+            </Text>
+
+            <Button title="Criar conta" variant="outline" />
+          </Center>
+        </VStack>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
