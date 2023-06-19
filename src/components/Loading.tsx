a0771ae0626e867661a0995d6774b5b6937581f5
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-19 14:13:11 -0300

    style: Ajusted colors.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { Spinner, Center } from 'native-base'
 
 export function Loading() {
   return (
-    <Center flex={1}>
-      <Spinner />
+    <Center flex={1} bg="gray.700">
+      <Spinner color="green.500" />
     </Center>
   )
 }
