b50de92999f13922b4ba71d7b26d5baf5b7988fe
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-20 17:38:07 -0300

    feat: Created Routes.

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { AuthRoutes } from './auth.routes'
+
+export function Routes() {
+  return (
+    <NavigationContainer>
+      <AuthRoutes />
+    </NavigationContainer>
+  )
+}
