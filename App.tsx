19660b0272e48789ef747a40cf2fcada60b0cf2e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-20 17:38:32 -0300

    feat: Add routes in app.

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 import { THEME } from './src/theme'
 
 import { Loading } from '@components/Loading'
-import { SignIn } from '@screens/SignIn'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -23,7 +23,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
