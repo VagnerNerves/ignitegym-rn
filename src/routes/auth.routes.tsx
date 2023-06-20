5e3d5d6ee6e326fc86f80361ef10028744703414
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-20 17:40:01 -0300

    feadt: adjust for header for false.

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -17,7 +17,7 @@ const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>()
 
 export function AuthRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="signIn" component={SignIn} />
       <Screen name="signUp" component={SignUp} />
     </Navigator>
