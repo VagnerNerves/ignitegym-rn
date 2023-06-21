1242976919a7875d49f9e07b0611bb6cded18e7d
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-21 10:23:19 -0300

    feat: Remove Header Route.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -21,7 +21,7 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="history" component={History} />
       <Screen name="profile" component={Profile} />
