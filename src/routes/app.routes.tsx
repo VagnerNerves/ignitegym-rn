66aff122b0dcb285bd7f991e58a7da44012fd801
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-21 10:21:01 -0300

    feat: Created type this app routes.

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,31 @@
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp
+} from '@react-navigation/bottom-tabs'
+
+import { Home } from '@screens/Home'
+import { History } from '@screens/History'
+import { Profile } from '@screens/Profile'
+import { Exercise } from '@screens/Exercise'
+
+type AppRoutes = {
+  home: undefined
+  history: undefined
+  profile: undefined
+  exercise: undefined
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  )
+}
