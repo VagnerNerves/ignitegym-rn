9272dbf7796333d8aeeaadd78fce3a2d9c7ca0e1
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-21 10:54:28 -0300

    feat: Add Color active and inactive on tab nar.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -25,12 +25,19 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
-  const { sizes } = useTheme()
+  const { sizes, colors } = useTheme()
 
   const iconSize = sizes[6]
 
   return (
-    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.green[500],
+        tabBarInactiveTintColor: colors.gray[200]
+      }}
+    >
       <Screen
         name="home"
         component={Home}
