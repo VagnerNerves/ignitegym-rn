c0fe0fa3513e702afe2322c894b7e5fa34acc20e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-22 10:48:29 -0300

    feat: Add Groups in screen Home.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
+import { VStack, FlatList } from 'native-base'
+
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
 
 export function Home() {
+  const [groups, setGrous] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSetected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSetected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
