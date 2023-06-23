f3701ec685ab7a21ea10195f2d71205abd6fdaf0
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-23 10:40:42 -0300

    feat: Add Photo and Skleton for loading.

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,37 @@
-import { VStack } from 'native-base'
+import { useState } from 'react'
+import { Center, ScrollView, VStack, Skeleton } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/VagnerNerves.png' }}
+              alt="Foto do Usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
