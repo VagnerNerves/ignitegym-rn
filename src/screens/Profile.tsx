db9f4afea357bce6e1a03d864b47393c7e6748f7
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-23 10:49:36 -0300

    feat: Add Button change photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { Center, ScrollView, VStack, Skeleton } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
@@ -30,6 +31,18 @@ export function Profile() {
               size={PHOTO_SIZE}
             />
           )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
