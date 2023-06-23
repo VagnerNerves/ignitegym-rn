6ac59d38a0d8a074ee9b05f567e58cbea521563b
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-23 14:19:48 -0300

    feat: Finalized layout.

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,11 +1,15 @@
 import { TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-import { HStack, Heading, Icon, Image, Text, VStack } from 'native-base'
+import { Box, HStack, Heading, Icon, Image, Text, VStack } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 import BodySvg from '@assets/body.svg'
+import SeriesSvg from '@assets/series.svg'
+import RepetitionsSvg from '@assets/repetitions.svg'
+
+import { Button } from '@components/Button'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -52,6 +56,30 @@ export function Exercise() {
           resizeMode="cover"
           rounded="lg"
         />
+
+        <Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack
+            alignItems="center"
+            justifyContent="space-around"
+            mb={6}
+            mt={5}
+          >
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 Séries
+              </Text>
+            </HStack>
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetições
+              </Text>
+            </HStack>
+          </HStack>
+
+          <Button title="Marcar como realizado" />
+        </Box>
       </VStack>
     </VStack>
   )
