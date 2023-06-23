0a27b7d4175503e93c4d6a67f7d3ae27db86eebc
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-23 11:54:20 -0300

    feat: Add Header on Screen.

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,44 @@
-import { Center, Text } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { HStack, Heading, Icon, Text, VStack } from 'native-base'
+import { Feather } from '@expo/vector-icons'
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import BodySvg from '@assets/body.svg'
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada Frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   )
 }
