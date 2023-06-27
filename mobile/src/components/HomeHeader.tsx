e8157cdcf34f5f841475587cb650dd689169341e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-27 10:11:59 -0300

    feat: Ajusted for get data user in storage and view in home header.

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -2,13 +2,19 @@ import { TouchableOpacity } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { HStack, Heading, Text, VStack, Icon } from 'native-base'
 
+import { useAuth } from '@hooks/useAuth'
+
+import defaultUserPhotoImg from '@assets/userPhotoDefault.png'
+
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
+  const { user } = useAuth()
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-        source={{ uri: 'https://github.com/VagnerNerves.png' }}
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
         alt="Imagem do Usuário"
         size={16}
         mr={4}
@@ -19,7 +25,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Vagner
+          {user.name}
         </Heading>
       </VStack>
 
