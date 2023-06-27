1d17deddffc2936d54420022a29b41c4c066ddc2
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-27 08:48:48 -0300

    feat: Add save user in storage.

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, createContext, useState } from 'react'
 
+import { storageUserSave } from '@storage/storageUser'
+
 import { api } from '@services/api'
 
 import { UserDTO } from '@dtos/UserDTO'
@@ -26,6 +28,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
