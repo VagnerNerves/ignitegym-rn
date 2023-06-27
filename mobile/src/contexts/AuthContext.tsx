06991f86c5e7806f717dedc10ff7cd46b1d7126f
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-27 08:26:38 -0300

    feat: Add function signIn.

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
 import { ReactNode, createContext, useState } from 'react'
 
+import { api } from '@services/api'
+
 import { UserDTO } from '@dtos/UserDTO'
 
 export type AuthContextDataProps = {
   user: UserDTO
+  signIn: (email: string, password: string) => Promise<void>
 }
 
 type AuthContextProviderProps = {
@@ -15,17 +18,25 @@ export const AuthContext = createContext<AuthContextDataProps>(
 )
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: '1',
-    name: 'Vagner Nerves',
-    email: 'vagnernervessantos@gmail.com',
-    avatar: 'https://github.com/VagnerNerves.png'
-  })
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password })
+
+      if (data.user) {
+        setUser(data.user)
+      }
+    } catch (error) {
+      throw error
+    }
+  }
 
   return (
     <AuthContext.Provider
       value={{
-        user
+        user,
+        signIn
       }}
     >
       {children}
