fd001952d8596f93f40ef9c20bf3ebda1edb5cad
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-27 10:30:19 -0300

    feat: Add signOut.

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -9,7 +9,7 @@ import defaultUserPhotoImg from '@assets/userPhotoDefault.png'
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
-  const { user } = useAuth()
+  const { user, signOut } = useAuth()
 
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
@@ -29,7 +29,7 @@ export function HomeHeader() {
         </Heading>
       </VStack>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={signOut}>
         <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
       </TouchableOpacity>
     </HStack>
