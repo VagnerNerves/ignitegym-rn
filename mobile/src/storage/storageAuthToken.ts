fac0e53fd5a75ba7baf01d6173097c48610cb8c3
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-28 10:06:19 -0300

    feat: Created function for get token.

--- mobile/src/storage/storageAuthToken.ts
+++ mobile/src/storage/storageAuthToken.ts
@@ -5,3 +5,9 @@ import { AUTH_TOKE_STORAGE } from '@storage/storageConfig'
 export async function storageAuthTokenSave(token: string) {
   await AsyncStorage.setItem(AUTH_TOKE_STORAGE, token)
 }
+
+export async function storageAuthTokenGet() {
+  const token = await AsyncStorage.getItem(AUTH_TOKE_STORAGE)
+
+  return token
+}
