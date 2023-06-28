16486765b8bd4aff3c4de2896a195d84b0f8d656
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-28 10:10:17 -0300

    feat: Ajusted name token and create function remove token.

--- mobile/src/storage/storageAuthToken.ts
+++ mobile/src/storage/storageAuthToken.ts
@@ -1,13 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { AUTH_TOKE_STORAGE } from '@storage/storageConfig'
+import { AUTH_TOKEN_STORAGE } from '@storage/storageConfig'
 
 export async function storageAuthTokenSave(token: string) {
-  await AsyncStorage.setItem(AUTH_TOKE_STORAGE, token)
+  await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token)
 }
 
 export async function storageAuthTokenGet() {
-  const token = await AsyncStorage.getItem(AUTH_TOKE_STORAGE)
+  const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE)
 
   return token
+}
+
+export async function storageAuthTokenRemove() {
+  await AsyncStorage.removeItem(AUTH_TOKEN_STORAGE)
 }
