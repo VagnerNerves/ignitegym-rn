44bec3affbcc48f86d4c87b580d8c5af9f7e004c
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-28 08:59:17 -0300

    feat: Add to save token and save authorization in api.

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useEffect, useState } from 'react'
 
+import { storageAuthTokenSave } from '@storage/storageAuthToken'
 import {
   storageUserSave,
   storageUserGet,
@@ -29,13 +30,28 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+      setUser(userData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password })
 
-      if (data.user) {
-        setUser(data.user)
-        storageUserSave(data.user)
+      if (data.user && data.token) {
+        storageUserAndToken(data.user, data.token)
       }
     } catch (error) {
       throw error
