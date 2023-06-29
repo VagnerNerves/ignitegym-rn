bf50d33f6c439e309e3af59a8fa4b7e0b71ab7a6
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-29 17:37:12 -0300

    feat: Ajusted for see image thumb.

--- mobile/src/components/ExerciseCard.tsx
+++ mobile/src/components/ExerciseCard.tsx
@@ -2,6 +2,8 @@ import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 import { HStack, Heading, Image, Text, VStack, Icon } from 'native-base'
 import { Entypo } from '@expo/vector-icons'
 
+import { api } from '@services/api'
+
 import { ExerciseDTO } from '@dtos/ExerciseDTO'
 
 type Props = TouchableOpacityProps & {
@@ -21,7 +23,7 @@ export function ExerciseCard({ data, ...rest }: Props) {
       >
         <Image
           source={{
-            uri: 'https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg'
+            uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}`
           }}
           alt="Imagem do exercício"
           w={16}
