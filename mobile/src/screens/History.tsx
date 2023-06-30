9a65d4f9a77f841986c6d47c795c63e9fc5d912a
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-29 21:27:25 -0300

    feat: Ajusted for set data for component history card.

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -50,7 +50,7 @@ export function History() {
       <SectionList
         sections={exercises}
         keyExtractor={item => item.id}
-        renderItem={({ item }) => <HistoryCard />}
+        renderItem={({ item }) => <HistoryCard data={item} />}
         renderSectionHeader={({ section }) => (
           <Heading
             color="gray.200"
