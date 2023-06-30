e438736936d860b3a8b9ebac36e3db87f2488cf6
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-29 21:09:52 -0300

    feat: Created get history on DB.

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,20 +1,47 @@
-import { useState } from 'react'
-import { Heading, VStack, SectionList, Text } from 'native-base'
+import { useCallback, useState } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base'
+
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { HistoryCard } from '@components/HistoryCard'
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    {
-      title: '26.08.22',
-      data: ['Puxada frontal', 'Remada unilateral']
-    },
-    {
-      title: '27.08.22',
-      data: ['Puxada frontal']
+  const [isLoading, setIsLoading] = useState(true)
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
+
+  const toast = useToast()
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get('/history')
+      setExercises(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar o histórico.'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
     }
-  ])
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory()
+    }, [])
+  )
 
   return (
     <VStack flex={1}>
@@ -22,7 +49,7 @@ export function History() {
 
       <SectionList
         sections={exercises}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => <HistoryCard />}
         renderSectionHeader={({ section }) => (
           <Heading
