70908bb91724f9c5131d2dda5e9c22ba78bfcd39
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-30 21:21:22 -0300

    feat: Ajusted the phot url.

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -2,6 +2,7 @@ import { TouchableOpacity } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { HStack, Heading, Text, VStack, Icon } from 'native-base'
 
+import { api } from '@services/api'
 import { useAuth } from '@hooks/useAuth'
 
 import defaultUserPhotoImg from '@assets/userPhotoDefault.png'
@@ -14,7 +15,11 @@ export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
+        source={
+          user.avatar
+            ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` }
+            : defaultUserPhotoImg
+        }
         alt="Imagem do Usuário"
         size={16}
         mr={4}
