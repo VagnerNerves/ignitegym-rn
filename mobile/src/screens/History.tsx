fefb06ac9cbc8ca9e26173271bb9ab75a92876e6
Vagner Nerves <vagnernervessantos@gmail.com>
2023-07-02 15:58:31 -0300

    feat: Ajusted header sectionlist for IOS.

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -80,6 +80,7 @@ export function History() {
             </Text>
           )}
           showsVerticalScrollIndicator={false}
+          stickySectionHeadersEnabled={false}
         />
       )}
     </VStack>
